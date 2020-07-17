3344dbf5be477e489501ed1837853154b0ae46f5
astratonova <68403149+astratonova@users.noreply.github.com>
2020-07-17 03:01:27 +0300

    Commit from IDEA

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -1,7 +1,7 @@
 public class MyFirstProgram {
   
   public static void main(String[] args) {
-    System.out.println("I'm smarter than a monkey");
+    System.out.println("I'm smarter than a monkey:)");
   }
 
 }
