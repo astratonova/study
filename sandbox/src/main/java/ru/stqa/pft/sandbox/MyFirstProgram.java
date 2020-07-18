9141c92dd30aa074f4426d42c19215c48fc76122
astratonova <68403149+astratonova@users.noreply.github.com>
2020-07-18 16:32:14 +0300

    Добавлен метод distance в класс Point
    используется экземпляр класса для которого вызван метод, а в аргументах передается второй экземпляр класса

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,19 +4,19 @@ public class MyFirstProgram {
 
   public static void main(String[] args) {
     System.out.println("I'm smarter than a monkey:)");
-    double x1=3;
-    double x2=0;
-    double y1=4;
-    double y2=0;
-    Point p1 = new Point(x1,y1);
-    Point p2 = new Point(x2,y2);
-    double d = distance(p1,p2);
-    System.out.println("Расстояние между 2 точками с координатами"+"("+x1+";"+y1+")"+"и"+"("+x2+";"+y2+")"+"="+d);
+    double x1 = 3;
+    double x2 = 0;
+    double y1 = 2;
+    double y2 = -2;
+    Point p1 = new Point(x1, y1);
+    Point p2 = new Point(x2, y2);
+    //выводится расстояние между 2 точками с вызовом функции  public static double distance(Point p1, Point p2)
+    System.out.println("Расстояние между 2 точками с координатами" + "(" + x1 + ";" + y1 + ")" + "и" + "(" + x2 + ";" + y2 + ")" + "=" + distance(p1, p2));
+    //выводится расстояние между 2 точками с вызовом метода public double distance(Point p2) класса "Point"
+    System.out.println("Расстояние между 2 точками с координатами" + "(" + p1.x + ";" + p1.y + ")" + "и" + "(" + p2.x + ";" + p2.y + ")" + "=" + p1.distance(p2));
   }
 
   public static double distance(Point p1, Point p2) {
-    double d;
-    d = Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
-    return d;
+    return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
   }
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -5,8 +5,13 @@ public class Point {
   public double x;
   public double y;
 
-  public Point(double x, double y){
-    this.x=x;
-    this.y=y;
+  public Point(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  //используется экземпляр класса, для которого вызван метод, а в аргументах передается второй экземпляр класса
+  public double distance(Point p2) {
+    return Math.sqrt(Math.pow(this.x - p2.x, 2) + Math.pow(this.y - p2.y, 2));
   }
 }
