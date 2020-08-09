8f175d0607416db4a8705e3f9bb44e839089aff6
astratonova <68403149+astratonova@users.noreply.github.com>
2020-08-09 21:14:25 +0300

    Добавлен поиск максимально элемента с помощью лямбда выражения

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -12,42 +12,30 @@ public class GroupCreationTests extends TestBase {
   @Test
   public void testGroupCreation() {
     app.getNavigationHelper().goToGroupPage();
-    List<GroupData> before= app.getGroupHelper().getGroupList();
-    GroupData group= new GroupData("group3", "group3(Logo)", "group3(Comment)");
+    List<GroupData> before = app.getGroupHelper().getGroupList();
+    GroupData group = new GroupData("group3", "group3(Logo)", "group3(Comment)");
     app.getGroupHelper().createNewGroup(group);
 
-    List<GroupData> after= app.getGroupHelper().getGroupList();
-    Assert.assertEquals(before.size() + 1,after.size());
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    Assert.assertEquals(before.size() + 1, after.size());
 
-    int max=0;
-    for (GroupData g : after){
-      if (max< g.getId()) {
-        max=g.getId();
-      }
-    }
-    group.setId(max);
+    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(group);
-    Assert.assertEquals(new HashSet<Object>(before),new HashSet<Object>(after));
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
 
   @Test
   public void testGroupCreationEmpty() {
     app.getNavigationHelper().goToGroupPage();
-    List<GroupData> before= app.getGroupHelper().getGroupList();
-    GroupData group= new GroupData("groupName", null, null);
+    List<GroupData> before = app.getGroupHelper().getGroupList();
+    GroupData group = new GroupData("groupName", null, null);
     app.getGroupHelper().createNewGroup(group);
 
-    List<GroupData> after= app.getGroupHelper().getGroupList();
-    Assert.assertEquals(before.size() + 1,after.size());
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    Assert.assertEquals(before.size() + 1, after.size());
 
-    int max=0;
-    for (GroupData g : after){
-      if (max< g.getId()) {
-        max=g.getId();
-      }
-    }
-    group.setId(max);
+    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(group);
-    Assert.assertEquals(new HashSet<Object>(before),new HashSet<Object>(after));
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
 }
